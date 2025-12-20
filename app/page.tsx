bb01e863805a5b7ba12dde996571fa8ebd2181e6
4liyat <dunter230@gmail.com>
2025-12-19 22:03:30 -0600

    Actualizando título del home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="p-10">
             <h1 className="text-4xl font-bold text-blue-600">
-                Hola Mundo
+                ¡Mi Portafolio está en Vivo! 🚀
             </h1>
             <p className="mt-4 text-xl">
                 Este es mi primer proyecto en Next.js 16.
